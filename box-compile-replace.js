e78f88dfdadcd1147138e73c2ee7aa42ab482ae7
Nimo <nimo.jser@gmail.com>
2017-04-27 14:11:57 +0800

    add dep

--- box-compile-replace.js
+++ box-compile-replace.js
@@ -1,3 +1,7 @@
+var json5 = require('json5')
+var hljs = require('highlight.js')
+var path = require('path')
+var fs = require('fs')
 module.exports = function (source, data, info) {
    var render = json5.parse(source)
    var jspath = path.join(path.dirname(info.filepath), render.file)
